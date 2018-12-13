ab86a1402adca4de3ffb847789dcd3572a1873e2
Steven Papalia <stevenapapalia@yahoo.com>
2018-12-13 14:49:28 -0800

    Initial Commit

---
+++ thirdRateMyLivingSpace/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  thirdRateMyLivingSpace
+//
+//  Created by Steven Papalia on 12/13/18.
+//  Copyright © 2018 Steven Papalia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
